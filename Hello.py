ff587110705be6e84f2c5afb630dbd4ff2d0d5e0
Arshad shemil k <160718560+Arshadshemilk@users.noreply.github.com>
2024-10-20 18:26:08 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -14,6 +14,7 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+from together import Together
 
 LOGGER = get_logger(__name__)
 
@@ -27,24 +28,21 @@ def run():
     st.write("# Welcome to Streamlit! 👋")
 
     st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
+    client = Together()
+    response = client.images.generate(
+        prompt="persian cat",
+        model="stabilityai/stable-diffusion-xl-base-1.0",
+        width=512,
+        height=512,
+        steps=40,
+        n=1,
+        seed=10000,
+        response_format="b64_json"
     )
+    print(response.data[0].b64_json)
+
+
+
 
 
 if __name__ == "__main__":
